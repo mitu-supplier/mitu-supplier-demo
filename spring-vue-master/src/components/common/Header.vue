a4971a577abe31c03098446727ab6182efeccde3
519978232 <519978232@qq.com>
2020-03-20 14:03:20 +0800

    11

--- spring-vue-master/src/components/common/Header.vue
+++ spring-vue-master/src/components/common/Header.vue
@@ -4,7 +4,7 @@
         <div class="collapse-btn" @click="collapseChage">
             <i class="el-icon-menu"></i>
         </div>
-        <div class="logo">积分时代</div>
+        <div class="logo">商户合作系统</div>
         <div class="header-right">
             <div class="header-user-con">
                 <!-- 全屏显示 -->
