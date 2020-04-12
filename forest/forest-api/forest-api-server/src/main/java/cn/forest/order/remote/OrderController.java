9019f036445a355df160705ccdb776c4169f8447
519978232 <519978232@qq.com>
2020-04-12 19:35:44 +0800

    11

--- forest/forest-api/forest-api-server/src/main/java/cn/forest/order/remote/OrderController.java
+++ forest/forest-api/forest-api-server/src/main/java/cn/forest/order/remote/OrderController.java
@@ -1,10 +1,11 @@
 package cn.forest.order.remote;
 
-import cn.forest.common.RemoteResponse;
-import cn.forest.order.dto.MerchantOrder;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import cn.forest.common.RemoteResponseForest;
+import cn.forest.order.dto.MerchantOrder;
 
 @RestController
 public class OrderController {
@@ -16,7 +17,7 @@ public class OrderController {
      * @return
      */
     @RequestMapping("/createMerchantOrder")
-    public RemoteResponse createMerchantOrder(@RequestBody MerchantOrder merchantOrder) {
+    public RemoteResponseForest createMerchantOrder(@RequestBody MerchantOrder merchantOrder) {
         return null;
     }
 }
